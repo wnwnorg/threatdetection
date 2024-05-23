8ac1931fef041beb54ed4ad91f87becd7d7467e8
Srikant Sarwa <8221522+ssarwa@users.noreply.github.com>
2024-05-23 12:30:42 -0700

    Update aws.tf

--- aws/aws.tf
+++ aws/aws.tf
@@ -1,1 +1,21 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "your-bucket-name"
+  acl    = "public-read"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "s3:GetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.public_bucket.bucket}/*"
+      },
+    ]
+  })
+}
