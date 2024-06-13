5dcc2be4c2dfa692a293d71692363b09f18e6f84
Srikant Sarwa <8221522+ssarwa@users.noreply.github.com>
2024-06-13 11:32:36 -0700

    Update aws.tf

--- aws/aws.tf
+++ aws/aws.tf
@@ -1,1 +1,14 @@
+resource "aws_s3_bucket" "b" {
+ bucket = "dev_s3_bucket"
+ acl    = "public"
 
+ tags = {
+   Name        = "Environment"
+   Environment = "Dev"
+ }
+}
+
+resource "aws_athena_database" "example" {
+  name   = "my_athena_database"
+  bucket = aws_s3_bucket.b.name
+}
